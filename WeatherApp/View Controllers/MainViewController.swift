35d2775bf26e8b1e83da536ac359387a436447ee
vsekerko@gmail.com <uasexe8x>
2022-07-01 21:06:46 +0300

    corrected the logic of sending notifications

--- WeatherApp/View Controllers/MainViewController.swift
+++ WeatherApp/View Controllers/MainViewController.swift
@@ -54,17 +54,31 @@ class MainViewController: UIViewController {
     
     //MARK: - Methods
     func weatherCheck(hourlyWeather: [HourlyWeatherData]) {
+        var index = 0
         for hour in hourlyWeather {
             guard let id = hour.weather?.first?.id, let time = hour.dt else { return }
-            
+            //Если плохая погода впервые или перерыв между уведомлениями больше 3х часов, отправляем новое уведомление
             switch id {
             case 200...232:
-                setLocalNotification(body: "soon thunderstorm", title: "Hey!", dateComponents: getDateComponentsFrom(date: time))
+                if index == 0 || index > 3 {
+                    setLocalNotification(body: "soon thunderstorm", title: "Hey!", dateComponents: getDateComponentsFrom(date: time))
+                    index = 1
+                }
+                index += 1
             case 500...531:
-                setLocalNotification(body: "soon rain", title: "Hey!", dateComponents: getDateComponentsFrom(date: time))
+                if index == 0 || index > 3 {
+                    setLocalNotification(body: "soon rain", title: "Hey!", dateComponents: getDateComponentsFrom(date: time))
+                    index = 1
+                }
+                index += 1
             case 600...622:
-                setLocalNotification(body: "soon snow", title: "Hey!", dateComponents: getDateComponentsFrom(date: time))
-            default: break
+                if index == 0 || index > 3 {
+                    setLocalNotification(body: "soon snow", title: "Hey!", dateComponents: getDateComponentsFrom(date: time))
+                    index = 1
+                }
+                index += 1
+            default:
+                index += 1
             }
         }
     }
